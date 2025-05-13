3cd478d38db2e1d937e6d54c3581fccab5432ded
luqmanaru <hanifluqmanulhakim20@gmail.com>
2025-05-13 16:20:20 +0700

    Create perkenalan.cpp

---
+++ perkenalan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hai semua" << endl;
+    cout << "Saya Hanif" << endl;
+    cout << "Jurusan saya Teknik Informatika" << endl;
+    return 0;
+}
